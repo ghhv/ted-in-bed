ddaef669e6b6cad7d1c210f3c6fbc32b3c114fa4
Gawain Lynch <gawain.lynch@gmail.com>
2013-04-01 21:57:48 +0100

    Initial release

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Plugin Name ===
+Contributors: gawain
+Tags: embed, oembed, TED
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enable TED video URLs to be embedded in WordPress via the internal oEmbed API
+
+== Description ==
+
+Curl up with your TEDdy bear and laptop and watch TED talks directly from your WordPress blog 
+
+Enable use of either TED talk URLs or [ted] short code.
+
+Format for shortcode is [ted id=X height=Y width=Z]
+ * id     [required] - Can be found by clicking the 'Embed' button on the TED talks page
+ * height [optional] - Desired height in pixels
+ * width  [optional] - Desired width in pixels
+
+
+== Installation ==
+
+1. Unzip to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place [ted] shortcode or talk URLs into posts
+
+== Frequently Asked Questions ==
+
+= Does this work with TED playlists? =
+
+No, I'm afraid not.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ ted-in-bed.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Plugin Name: TED in bed
+ * Plugin URI: http://gawainlynch.com/ted-in-bed
+ * Description: Enable TED video URLs to be embedded in WordPress via the internal oEmbed API
+ * Version: 1.0
+ * License: GPLv2 or later
+ * Author: Gawain Lynch
+ * Author URI: http://gawainlynch.com/
+ */
+
+/*
+ * Add TED oEmbed API JSON target
+ */
+wp_oembed_add_provider( 'http://www.ted.com/talks/*', 'http://www.ted.com/talks/oembed.json' );
+
+
+/**
+ * Set default height to a more sane 16:9
+ *
+ * @see wp_embed_defaults() in wp-includes/media.php
+ * @param $attrs - Array with 'height' and 'width' elements
+ * @return Array
+ */
+function tib_embed_defaults( $attrs )
+{
+	/*
+	 * WordPress for some reason defaults to an aspect ratio of 1:1.5
+	 *
+	 * The width defaults to the content width as specified by the theme. If the
+	 * theme does not specify a content width, then 500px is used.
+	 *
+	 * The default height is 1.5 times the width, or 1000px, whichever is smaller.
+	 *
+	 * For a ratio of 4:3 change the divisor to 1.33
+	 */
+	$ratio = apply_filters( 'tib_aspect_ratio_divisor', 1.77 );
+	$attrs['height'] = ceil( $attrs['width'] / $ratio );
+	return $attrs;
+}
+
+add_filter( 'embed_defaults', 'tib_embed_defaults', 10 );
+
+/**
+ * Handle [ted] shortcodes
+ *
+ * Shorcode parameters:
+ *   ~ id     - Numeric ID of TED talk
+ *   ~ width  - Width in pixels
+ *   ~ height - Height in pixels
+ *
+ * @param $attrs  - Array
+ * @return String - Resulting HTML
+ */
+function tib_shortcode_handler( $attrs )
+{
+    global $wp_embed;
+
+	// Height and width defaults
+	extract( shortcode_atts( wp_embed_defaults(), $attrs ) );
+
+    if ( empty( $attrs['id'] ) || ! is_numeric( ( $attrs['id'] ) ) )
+        return false;
+
+    $url = 'http://www.ted.com/talks/view/lang/eng/id/' . $attrs['id'];
+
+    return $wp_embed->shortcode( $attrs, $url );
+}
+
+add_shortcode( 'ted', 'tib_shortcode_handler' );
